800a5ee1fe7ae7880d00be620b94e11b6986a09f
p3lim-52096 <p3lim-52096@5fa57bb9-5a9b-438a-a66f-544cb7f2d95c>
2009-04-08 23:20:45 +0000

    Fixed various performance issues
    git-svn-id: svn://svn.wowinterface.com/oUF_ReadyCheck-418/trunk@5 5fa57bb9-5a9b-438a-a66f-544cb7f2d95c

--- oUF_ReadyCheck/oUF_ReadyCheck.lua
+++ oUF_ReadyCheck/oUF_ReadyCheck.lua
@@ -9,10 +9,17 @@
 
 --]]
 
+local GetReadyCheckStatus = GetReadyCheckStatus
 
-local OnFinishUpdate
+local statusTexture = {
+	notready = [=[Interface\RAIDFRAME\ReadyCheck-NotReady]=],
+	ready = [=[Interface\RAIDFRAME\ReadyCheck-Ready]=],
+	waiting = [=[Interface\RAIDFRAME\ReadyCheck-Waiting]=],
+}
+
+local OnUpdate
 do
-	function OnFinishUpdate(self, elapsed)
+	function OnUpdate(self, elapsed)
 		if(self.finish) then
 			self.finish = self.finish - elapsed
 			if(self.finish <= 0) then
@@ -24,14 +31,14 @@ do
 				self.fade = nil
 				self:SetScript('OnUpdate', nil)
 
-				for k,v in ipairs(oUF.objects) do
-					if(type(v) == 'table' and v.ReadyCheck) then
+				for k, v in next, oUF.objects do
+					if(v.ReadyCheck and v.unit == self.unit) then
 						v.ReadyCheck:Hide()
 					end
 				end
 			else
-				for k,v in ipairs(oUF.objects) do
-					if(type(v) == 'table' and v.ReadyCheck) then
+				for k, v in next, oUF.objects do
+					if(v.ReadyCheck and v.unit == self.unit) then
 						v.ReadyCheck:SetAlpha(self.fade / self.offset)
 					end
 				end
@@ -40,45 +47,32 @@ do
 	end
 end
 
-local function READY_CHECK(self, event, name)
+local function Update(self)
 	if(not IsRaidLeader() and not IsRaidOfficer() and not IsPartyLeader()) then return end
 
 	local texture = self.ReadyCheck
-	if(UnitName(self.unit) == name) then
-		texture:SetTexture([=[Interface\RAIDFRAME\ReadyCheck-Ready]=])
-	else
-		texture:SetTexture([=[Interface\RAIDFRAME\ReadyCheck-Waiting]=])
-	end
-
+	texture:SetTexture(statusTexture[GetReadyCheckStatus(self.unit)])
 	texture:SetAlpha(1)
 	texture:Show()
 end
 
-local function READY_CHECK_CONFIRM(self, event, index, status)
-	if(self.id ~= tostring(index)) then return end
+local function PrepareFade(self)
+	local readycheck = self.ReadyCheck
+	local dummy = readycheck.dummy
 
-	local texture = self.ReadyCheck
-	if(status and status == 1) then
-		texture:SetTexture([=[Interface\RAIDFRAME\ReadyCheck-Ready]=])
-	else
-		texture:SetTexture([=[Interface\RAIDFRAME\ReadyCheck-NotReady]=])
-	end
-end
-
-local function READY_CHECK_FINISHED(self)
-	local rc = self.ReadyCheck
-	rc.dummy.finish = rc.delayTime or 10
-	rc.dummy.fade = rc.fadeTime or 1.5
-	rc.dummy.offset = rc.fadeTime or 1.5
-	rc.dummy:SetScript('OnUpdate', OnFinishUpdate)
+	dummy.unit = self.unit
+	dummy.finish = readycheck.delayTime or 10
+	dummy.fade = readycheck.fadeTime or 1.5
+	dummy.offset = readycheck.fadeTime or 1.5
+	dummy:SetScript('OnUpdate', OnUpdate)
 end
 
 local function Enable(self)
 	local readycheck = self.ReadyCheck
 	if(readycheck) then
-		self:RegisterEvent('READY_CHECK', READY_CHECK)
-		self:RegisterEvent('READY_CHECK_CONFIRM', READY_CHECK_CONFIRM)
-		self:RegisterEvent('READY_CHECK_FINISHED', READY_CHECK_FINISHED)
+		self:RegisterEvent('READY_CHECK', Update)
+		self:RegisterEvent('READY_CHECK_CONFIRM', Update)
+		self:RegisterEvent('READY_CHECK_FINISHED', PrepareFade)
 
 		readycheck.dummy = CreateFrame('Frame', nil, self)
 
@@ -88,10 +82,10 @@ end
 
 local function Disable(self)
 	if(self.ReadyCheck) then
-		self:UnregisterEvent('READY_CHECK', READY_CHECK)
-		self:UnregisterEvent('READY_CHECK_CONFIRM', READY_CHECK_CONFIRM)
-		self:UnregisterEvent('READY_CHECK_FINISHED', READY_CHECK_FINISHED)
+		self:UnregisterEvent('READY_CHECK', Update)
+		self:UnregisterEvent('READY_CHECK_CONFIRM', Update)
+		self:UnregisterEvent('READY_CHECK_FINISHED', PrepareFade)
 	end
 end
 
-oUF:AddElement('ReadyCheck', nil, Enable, Disable)
+oUF:AddElement('ReadyCheck', Update, Enable, Disable)
